2a55a9e68517b20c0efa66ec9d0649ed5410a540
Zack <77754097+Zacknac@users.noreply.github.com>
2021-05-07 19:41:37 -0600

    kekw
    kekw

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -1,15 +1,15 @@
-using Godot;
+using Godot; // at least it's not unity
 
-public class Main : Spatial
+public class Main : Spatial // im not even going to pretend i know what this means
 {
-    Classes Classes = new Classes();
-    public override void _Ready()
+    Classes Classes = new Classes(); // classes classes classes classes classes
+    public override void _Ready() // you're not _Ready() for this bad pun
     {
-        GD.Print("Team Fortress 3");
-        GD.Print("Created by Blocks_n_more");
+        GD.Print("Team Fortress 3"); // valve's lawyers approve
+        GD.Print("Created by Blocks_n_more"); // ha ha not anymore -zanac
     }
-    public void LoadFiles()
+    public void LoadFiles() // gee i wonder what this does
     {
-        new Sorse(this);
+        new Sorse(this); // achieved with sorse
     }
 }
